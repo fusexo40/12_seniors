28698937ee1d5c40378fd6410e19863acfb805eb
Svyatoslav Frolov <slava.frolov.614@gmail.com>
2024-02-22 22:54:07 +0300

    похуй

---
+++ src/keyboards.py
@@ -0,0 +1,46 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.filters.callback_data import CallbackData
+
+
+class Pagination(CallbackData, prefix="pag"):
+    action: str
+    page: int
+
+
+def paginator(page: int = 0):
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(text="⬅", callback_data=Pagination(action="prev", page=page).pack()),
+        InlineKeyboardButton(text="➡", callback_data=Pagination(action="next", page=page).pack()),
+        width=2
+
+    )
+    return builder.as_markup()
+
+
+def class_kb():
+    items = [
+        "1", "2", "3",
+        "4", "5", "6",
+        "7", "8", "9",
+        "10", "11"
+    ]
+    builder = ReplyKeyboardBuilder()
+    [builder.button(text=item) for item in items]
+    builder.adjust(*[3] * 4)
+    return builder.as_markup(resize_keyboard=True)
+
+
+main_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Наше сообщество", url="tg://resolve?domain=dz_helper_chanel")
+        ]
+    ]
+)
